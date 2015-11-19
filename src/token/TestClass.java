4626a40e88383feb9268bf38a26fc91f0bee6c47
liandahu <liandahu@jd.com>
2015-11-19 12:00:28 +0800

    增加一个测试类

---
+++ src/token/TestClass.java
@@ -0,0 +1,14 @@
+package token;
+
+/**
+ * 增加一个测试类
+ * 
+ * @author liandahu
+ *
+ */
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
+}
